e0254903c1cfc35490f8f8775a2b9cf185c21d73
griteach <griteach22@gmail.com>
2024-01-20 22:33:39 +0900

    design

--- src/components/post-tweet-form.tsx
+++ src/components/post-tweet-form.tsx
@@ -1,10 +1,56 @@
 import styled from "styled-components";
 
-const Form = styled.form``;
-const TextArea = styled.textarea``;
-const AttachFileButton = styled.label``;
-const AttachFileInput = styled.input``;
-const SubmitBtn = styled.input``;
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+const TextArea = styled.textarea`
+  border: 2px solid white;
+  padding: 20px;
+  border-radius: 20px;
+  font-size: 16px;
+  color: white;
+  background-color: black;
+  width: 100%;
+  resize: none;
+  &::placeholder {
+    font-size: 16px;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI",
+      Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue",
+      sans-serif;
+  }
+  &:focus {
+    outline: none;
+    border-color: #1d9bf0;
+  }
+`;
+const AttachFileButton = styled.label`
+  padding: 10px 0px;
+  color: #1d9bf0;
+  text-align: center;
+  border-radius: 20px;
+  border: 1px solid #1d9bf0;
+  font-size: 14px;
+  font-weight: 600;
+  cursor: pointer;
+`;
+const AttachFileInput = styled.input`
+  display: none;
+`;
+const SubmitBtn = styled.input`
+  background-color: #1d9bf0;
+  color: white;
+  border: none;
+  padding: 10px 0px;
+  border-radius: 20px;
+  font-size: 16px;
+  cursor: pointer;
+  &:hover,
+  &:active {
+    opacity: 0.8;
+  }
+`;
 
 export default function PostTweetForm() {
   return (
@@ -12,7 +58,7 @@ export default function PostTweetForm() {
       <TextArea placeholder="What is happening?" />
       <AttachFileButton htmlFor="file">Add photo</AttachFileButton>
       <AttachFileInput type="file" id="file" accept="image/*" />
-      <SubmitBtn />
+      <SubmitBtn type="submit" value="Post Tweet" />
     </Form>
   );
 }
