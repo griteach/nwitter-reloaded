8331608a8ce5d94d61953d77e0389e96b5f11baa
griteach <griteach22@gmail.com>
2024-01-22 23:58:32 +0900

    code challange (edit) ing...

--- src/components/tweet.tsx
+++ src/components/tweet.tsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import { ITweet } from "./timeline";
 import { auth, db, storage } from "../firebase";
-import { deleteDoc, doc } from "firebase/firestore";
+import { deleteDoc, doc, updateDoc } from "firebase/firestore";
 import { deleteObject, ref } from "firebase/storage";
 
 const Wrapper = styled.div`
@@ -27,15 +27,22 @@ const Payload = styled.p`
 `;
 
 const DeleteBtn = styled.button`
-  background-color: tomato;
-  color: white;
-  font-weight: 600;
-  font-size: 12px;
-  border: 0;
-  padding: 5px 10px;
-  text-transform: uppercase;
-  border-radius: 5px;
   cursor: pointer;
+  background-color: transparent;
+  border: 0;
+  svg {
+    width: 20px;
+    color: tomato;
+  }
+`;
+const UpdateBtn = styled.button`
+  cursor: pointer;
+  background-color: transparent;
+  border: 0;
+  svg {
+    width: 20px;
+    color: tomato;
+  }
 `;
 
 export default function Tweet({ userName, photo, tweet, userId, id }: ITweet) {
@@ -55,13 +62,58 @@ export default function Tweet({ userName, photo, tweet, userId, id }: ITweet) {
     } finally {
     }
   };
+  const onUpdate = async () => {
+    const ok = confirm("Are you sure you want to update this tweet?");
+
+    if (!ok || user?.uid !== userId) return;
+    try {
+      await updateDoc();
+    } catch (error) {
+      console.log(error);
+    } finally {
+    }
+  };
+
   return (
     <Wrapper>
       <Column>
         <UserName>{userName}</UserName>
         <Payload>{tweet}</Payload>
         {user?.uid === userId ? (
-          <DeleteBtn onClick={onDelete}>Delete</DeleteBtn>
+          <>
+            <DeleteBtn onClick={onDelete}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                />
+              </svg>
+            </DeleteBtn>
+            <UpdateBtn onClick={onUpdate}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125"
+                />
+              </svg>
+            </UpdateBtn>
+          </>
         ) : null}
       </Column>
       <Column>{photo ? <Photo src={photo} /> : null}</Column>
